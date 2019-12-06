6cde0b06b3376c9de2e9115ad2ec386b73eac3c3
Guilherme Pedrozo Abacherli <guilherme.abacherli@fatec.sp.gov.br>
2019-12-06 18:42:52 -0300

    Adicionado notistack como snackbar de notificação padrão.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React from 'react';
 import "./styles/global";
 import Routes from "./routes";
 import GlobalStyle from './styles/global';
+import { SnackbarProvider } from "notistack";
+import { IconButton } from "@material-ui/core";
+import CloseIcon from "@material-ui/icons/Close";
 
 const App = () => (
     <React.Fragment>
@@ -10,4 +13,22 @@ const App = () => (
     </React.Fragment>
 );
 
-export default App;
+const notistackRef = React.createRef();
+
+const AppIntegratedNotistack = () => (
+    <SnackbarProvider maxSnack={3}
+        ref={notistackRef}
+        anchorOrigin={{ vertical: "top", horizontal: "right", }}
+        action={key => (
+            <IconButton
+                style={{ color: "white" }}
+                onClick={() => notistackRef.current.closeSnackbar(key)}
+            >
+                <CloseIcon fontSize="small" />
+            </IconButton>)}
+    >
+        <App />
+    </SnackbarProvider >
+);
+
+export default AppIntegratedNotistack;
