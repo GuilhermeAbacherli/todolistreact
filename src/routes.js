9d943016cf76b898c358e8d9d9a2db846106284e
Guilherme Pedrozo Abacherli <guilherme.abacherli@fatec.sp.gov.br>
2019-12-06 18:47:39 -0300

    Ajuste nas rotas para as páginas.

--- src/routes.js
+++ src/routes.js
@@ -2,7 +2,10 @@ import React from "react";
 import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 
 import { isAuthenticated } from "./services/auth";
-import SignUp from "./pages/PLogin";
+import LoginPage from "./pages/PLogin";
+import RegisterPage from "./pages/PRegister";
+import HomePage from "./pages/PHome";
+import AboutPage from "./pages/PAbout";
 
 const PrivateRoute = ({ component: Component, ...rest }) => (
     <Route
@@ -20,9 +23,10 @@ const PrivateRoute = ({ component: Component, ...rest }) => (
 const Routes = () => (
     <BrowserRouter>
         <Switch>
-            <Route exact path="/" component={() => <h1>Login</h1>} />
-            <Route path="/signup" component={SignUp} />
-            <PrivateRoute path="/app" component={() => <h1>App</h1>} />
+            <Route exact path="/" component={LoginPage} />
+            <Route path="/register" component={RegisterPage} />
+            <PrivateRoute path="/home" component={HomePage} />
+            <PrivateRoute path="/about" component={AboutPage} />
             <Route path="*" component={() => <h1>Page not found</h1>} />
         </Switch>
     </BrowserRouter>
